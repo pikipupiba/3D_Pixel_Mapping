298f0e1f5733172e61a1af93415293178827517a
pikipupiba <jjjsutter@gmail.com>
2021-05-18 18:38:28 -0500

    FUCK YEA page working

---
+++ app/src/controllers/map_leds.js
@@ -0,0 +1,8 @@
+
+
+const map_leds = (req, res, next) => {
+
+}
+
+
+export { map_leds as default }
--- app/src/router.js
+++ app/src/router.js
@@ -2,4 +2,18 @@ import express from "express";
 
 const router = express.Router();
 
-router.get()
+router.get("/", (req, res, next) => {
+    //res.sendFile(path.join(__dirname, '/view/index.html'));
+    res.send("FUCK YEA");
+});
+
+import map_leds from "./controllers/map_leds.js";
+
+router.post("/map_leds", map_leds);
+
+import upload from "./controllers/upload.js";
+
+router.post("/upload", upload);
+
+
+export { router as default };
